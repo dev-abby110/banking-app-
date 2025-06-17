c254df6febc1f2090ccdab04cc22002be7b14bd9
Abbas aziz <92018896+dev-abby110@users.noreply.github.com>
2025-06-17 10:29:04 +0530

    Delete Mini_Banking_project directory

--- Mini_Banking_project/Banking/transactions.py
+++
@@ -1,6 +0,0 @@
-# from Banking.account import BankAccount, SavingsAccount, CurrentAccount
-def deposit(account, amount):
-    return account.deposit(amount)
-
-def withdraw(account, amount):
-    return account.withdraw(amount)
